222a835acd57ca11de2d5a2310648fc054b4a9a4
phaitanduocxuyen <tup22072006@gmail.com>
2025-09-24 16:01:50 +0700

    innit commit

--- Conditional_statements/natural_number_check.java
+++ Conditional_statements/natural_number_check.java
@@ -1,4 +1,14 @@
 package Conditional_statements;
+import java.util.Scanner;
 
 public class natural_number_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N >= 0) {
+            System.out.println("N la so tu nhien");
+        } else {
+            System.out.println("N khong phai so tu nhien");
+        }
+    }
 }
