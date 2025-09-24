5124d1f6b07de6f7aa4ab423647e614e395c3ce0
phaitanduocxuyen <tup22072006@gmail.com>
2025-09-24 17:10:40 +0700

    Add_Vd2,3

---
+++ Switch_Case_Command/VD3_DirectionFromCharacter.java
@@ -0,0 +1,18 @@
+package Switch_Case_Command;
+import java.util.Scanner;
+
+public class VD3_DirectionFromCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a direction character (N/S/E/W): ");
+        char ch = scanner.next().toUpperCase().charAt(0);
+
+        switch (ch) {
+            case 'N' -> System.out.println("Hướng Bắc");
+            case 'S' -> System.out.println("Hướng Nam");
+            case 'E' -> System.out.println("Hướng Tây");
+            case 'W' -> System.out.println("Hướng Đông");
+            default -> System.out.println("Ký tự không hợp lệ");
+        }
+    }
+}
