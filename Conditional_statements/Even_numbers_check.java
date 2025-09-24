1bcca195aa8a687b9d972ba9c932b69fe24cc71e
phaitanduocxuyen <tup22072006@gmail.com>
2025-09-24 16:10:51 +0700

    innit commit

---
+++ Conditional_statements/Even_numbers_check.java
@@ -0,0 +1,13 @@
+package Conditional_statements;
+import java.util.Scanner;
+public class Even_numbers_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N % 2 == 0) {
+            System.out.println("N la so chan");
+        } else {
+            System.out.println("N khong phai so chan");
+        }
+    }
+}
