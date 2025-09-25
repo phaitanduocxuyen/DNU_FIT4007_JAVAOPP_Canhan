8882c6d235b638d105110a54338459e94326def1
phaitanduocxuyen <tup22072006@gmail.com>
2025-09-25 13:51:01 +0700

    Add_Vd1,vd5loops

---
+++ Loops_in_Java/VD1_PrintPTIT20Times.java
@@ -0,0 +1,9 @@
+package Loops_in_Java;
+
+public class VD1_PrintPTIT20Times {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("PTIT");
+        }
+    }
+}
