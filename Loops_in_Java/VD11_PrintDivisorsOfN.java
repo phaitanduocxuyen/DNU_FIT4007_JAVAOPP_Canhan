5e95c806513e5c338ce3ac517d1f3baa129de963
phaitanduocxuyen <tup22072006@gmail.com>
2025-09-25 20:41:06 +0700

    Add_Vd10-vd12loops

---
+++ Loops_in_Java/VD11_PrintDivisorsOfN.java
@@ -0,0 +1,15 @@
+package Loops_in_Java;
+
+import java.util.Scanner;
+
+public class VD11_PrintDivisorsOfN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
