42bfdc17ec9e09bdb21db77d303d54c035b56aa0
phaitanduocxuyen <tup22072006@gmail.com>
2025-10-13 01:04:40 +0700

    Add_Vd7-VD13_Array

---
+++ Array_in_Java/VD7_MinMaxArray.java
@@ -0,0 +1,39 @@
+package Array_in_Java;
+
+import java.util.Scanner;
+
+public class VD7_MinMaxArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập N: ");
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("A[" + i + "] = ");
+            a[i] = sc.nextInt();
+        }
+
+        if (n > 0) {
+            int maxVal = a[0];
+            int minVal = a[0];
+
+            for (int i = 1; i < n; i++) {
+                if (a[i] > maxVal) {
+                    maxVal = a[i];
+                }
+                if (a[i] < minVal) {
+                    minVal = a[i];
+                }
+            }
+
+            System.out.println("Giá trị lớn nhất: " + maxVal);
+            System.out.println("Giá trị nhỏ nhất: " + minVal);
+        } else {
+            System.out.println("Mảng rỗng.");
+        }
+    }
+}
