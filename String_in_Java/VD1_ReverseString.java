6f0aa1aaec9fb6b1f870344dd549236d7bdb40ee
phaitanduocxuyen <tup22072006@gmail.com>
2025-10-13 18:13:37 +0700

    Add_Vd1-VD2_String

---
+++ String_in_Java/VD1_ReverseString.java
@@ -0,0 +1,17 @@
+package String_in_Java;
+
+import java.util.Scanner;
+
+public class VD1_ReverseString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập chuỗi: ");
+        String original = sc.nextLine();
+
+        StringBuilder reversed = new StringBuilder(original).reverse();
+
+        System.out.println("Chuỗi đảo ngược: " + reversed.toString());
+    }
+}
