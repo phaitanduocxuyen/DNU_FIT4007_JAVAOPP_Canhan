9850d2248e652626d26ac4a305c503c4c18fad69
phaitanduocxuyen <tup22072006@gmail.com>
2025-10-17 17:28:08 +0700

    Add_Vd4_method

---
+++ Method_programming/VD4_CheckEvenNumber.java
@@ -0,0 +1,22 @@
+package Method_programming;
+
+import java.util.Scanner;
+
+public class VD4_CheckEvenNumber {
+
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập một số nguyên: ");
+        int num = sc.nextInt();
+
+        if (isEven(num)) {
+            System.out.println(num + " là số chẵn.");
+        } else {
+            System.out.println(num + " KHÔNG là số chẵn.");
+        }
+    }
+}
