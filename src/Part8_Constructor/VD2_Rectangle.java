056d4df49ed0d1c5980388db26328f7430291f5b
phaitanduocxuyen <tup22072006@gmail.com>
2025-10-23 16:57:39 +0700

    add_bt

---
+++ src/Part8_Constructor/VD2_Rectangle.java
@@ -0,0 +1,38 @@
+package src.Part8_Constructor;
+
+public class VD2_Rectangle {
+    private double width;
+    private double height;
+
+    public VD2_Rectangle() {
+    }
+
+    public VD2_Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "VD2_Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
---
+++ src/Part8_Constructor/VD3_Circle.java
@@ -0,0 +1,27 @@
+package src.Part8_Constructor;
+
+public class VD3_Circle {
+    private double radius;
+
+    public VD3_Circle() {
+    }
+
+    public VD3_Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "VD3_Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
